5bec56031f1aa8ae7a11b4712bb4edb974d7120e
Ilia Feoktistov <ilia.a.feoktistov@gmail.com>
2025-04-14 18:53:41 +0700

    adding wait period for redis startup

--- pkg/resources/statefulsets/statefulset.go
+++ pkg/resources/statefulsets/statefulset.go
@@ -50,7 +50,7 @@ func NewStatefulSetForCR(cluster *redisv1alpha1.DistributedRedisCluster, ssName,
 			ServiceName: svcName,
 			Replicas:    &size,
 			UpdateStrategy: appsv1.StatefulSetUpdateStrategy{
-				Type: appsv1.RollingUpdateStatefulSetStrategyType,
+				Type: appsv1.OnDeleteStatefulSetStrategyType,
 			},
 			Selector: &metav1.LabelSelector{
 				MatchLabels: labels,
@@ -176,11 +176,20 @@ func ClusterHeadlessSvcName(name string, i int) string {
 
 func getRedisCommand(cluster *redisv1alpha1.DistributedRedisCluster, password *corev1.EnvVar) []string {
 	cmd := []string{
-		"/conf/fix-ip.sh",
-		"redis-server",
-		"/conf/redis.conf",
-		"--cluster-enabled yes",
-		"--cluster-config-file /data/nodes.conf",
+		"/bin/sh",
+		"-c",
+		`
+		/conf/fix-ip.sh
+		
+		# Check if this node has nodes.conf and might be a master
+		if [ -f "/data/nodes.conf" ] && grep -q "myself,master" /data/nodes.conf 2>/dev/null; then
+			echo "Master node , delaying startup for 20 seconds to allow failover..."
+			sleep 20
+			echo "Delay completed, starting Redis server"
+		fi
+		
+		redis-server /conf/redis.conf --cluster-enabled yes --cluster-config-file /data/nodes.conf
+		`,
 	}
 	if password != nil {
 		cmd = append(cmd, fmt.Sprintf("--requirepass '$(%s)'", redisv1alpha1.PasswordENV),
